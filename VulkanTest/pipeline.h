74dff7951b51fec855f212c22dcbbd04428ed317
Jameson Wright <a.jameson.wright@gmail.com>
2024-06-10 15:52:18 -0400

    Implement mipmaps

--- VulkanTest/pipeline.h
+++ VulkanTest/pipeline.h
@@ -136,6 +136,7 @@ namespace vd {
 		VkPipeline pipeline;
 		std::vector<VkFramebuffer> swapChainFrameBuffers;
 		VkCommandPool commandPool;
+		uint32_t mipLevels;
 		VkImage textureImage;
 		VkDeviceMemory textureImageMemory;
 		VkImageView textureImageView;
@@ -174,7 +175,7 @@ namespace vd {
 		void recreateSwapChain();
 		void cleanUpSwapChain();
 		void createImageViews();
-		VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+		VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels);
 		void createRenderPass();
 		void createDescriptorSetLayout();
 		void createGraphicsPipeline(const std::string& vertPath, const std::string& fragPath);
@@ -185,7 +186,8 @@ namespace vd {
 		VkFormat findSupportedFormat(const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features);
 		bool hasStencilComponent(VkFormat format);
 		void createTextureImage(const std::string& path);
-		void createImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+		void createImage(uint32_t width, uint32_t height, uint32_t mipLevels, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+		void generateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels);
 		void createTextureImageView();
 		void createTextureSampler();
 		void loadModel(const std::string& modelPath);
@@ -196,7 +198,7 @@ namespace vd {
 		void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
 		VkCommandBuffer beginSingleTimeCommands();
 		void endSingleTimeCommands(VkCommandBuffer commandBuffer);
-		void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout);
+		void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels);
 		void createSyncObjects();
 		void createUniformBuffers();
 		void createDescriptorPool();
